4cdd807c7e9bccf3b3bf69f103c533a109252b48
Oliver Forral <intrepion@gmail.com>
2021-12-23 16:05:01 -0700

    installs git if not found

--- setup-coding/src/main.rs
+++ setup-coding/src/main.rs
@@ -1,34 +1,59 @@
-use std::process::Command;
+use std::io::Error;
+use std::process::{Child, Command};
 
-fn check_for_tool(name: &str) {
-    let spawn = Command::new(name)
-        .arg("--version")
-        .spawn();
-
-    match spawn {
+fn check_process_status(message: &str, process: Result<Child, Error>) -> bool {
+    match process {
         Err(error) => {
-            println!("Error message: {}", error)
-        },
+            println!("process error message: {}", error);
+
+            false
+        }
         Ok(mut child) => {
             let exit_status = child.wait();
 
             match exit_status {
                 Err(error) => {
-                    println!("Error message: {}", error)
-                },
+                    println!("exit status error message: {}", error);
+
+                    false
+                }
                 Ok(status) => {
-                    println!("{}", status);
+                    println!("process status: {}", status);
+                    println!("{}", message);
+
+                    true
                 }
             }
-        },
-    }    
+        }
+    }
+}
+
+fn install_git() {
+    println!("\ninstalling tool: git");
+    let process = Command::new("apt")
+        .arg("install")
+        .arg("git-all")
+        .spawn();
+    
+    check_process_status("installed tool: git", process);
+}
+
+fn can_find_tool(tool_name: &str) -> bool {
+    println!("\nchecking for tool: {}", tool_name);
+    let process = Command::new(tool_name).arg("--version").spawn();
+
+    let message = format!("found tool: {}", tool_name);
+
+    check_process_status(&message, process)
 }
 
 fn main() {
-    check_for_tool("git");
-    check_for_tool("code");
-    check_for_tool("google-chrome");
-    check_for_tool("firefox");
-    check_for_tool("docker");
-    check_for_tool("docker-compose")
+    if !can_find_tool("git") {
+        install_git();
+    }
+    can_find_tool("code");
+    can_find_tool("google-chrome");
+    can_find_tool("firefox");
+    can_find_tool("docker");
+    can_find_tool("docker-compose");
 }
