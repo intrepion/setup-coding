fe350037c6d963ab6c8ad0906ac8d903c1b9bc1c
Oliver Forral <intrepion@gmail.com>
2021-12-23 03:47:10 -0700

    check for tools

--- setup-coding/src/main.rs
+++ setup-coding/src/main.rs
@@ -1,3 +1,34 @@
+use std::process::Command;
+
+fn check_for_tool(name: &str) {
+    let spawn = Command::new(name)
+        .arg("--version")
+        .spawn();
+
+    match spawn {
+        Err(error) => {
+            println!("Error message: {}", error)
+        },
+        Ok(mut child) => {
+            let exit_status = child.wait();
+
+            match exit_status {
+                Err(error) => {
+                    println!("Error message: {}", error)
+                },
+                Ok(status) => {
+                    println!("{}", status);
+                }
+            }
+        },
+    }    
+}
+
 fn main() {
-    println!("Hello, world!");
+    check_for_tool("git");
+    check_for_tool("code");
+    check_for_tool("google-chrome");
+    check_for_tool("firefox");
+    check_for_tool("docker");
+    check_for_tool("docker-compose")
 }
